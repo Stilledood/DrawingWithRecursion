6ac11ce38f6728969ac695c284148f0cf45ffe7b
Stilledood <corciova.f@gmail.com>
2023-02-18 16:57:40 +0200

    Sierpinski Triangle - function to actual draw a sierpinski trinagle

--- sierpinski.py
+++ sierpinski.py
@@ -1,9 +1,10 @@
 import turtle
 
-my_points = [[-100, -50], [0, 100], [100, -50]]
+my_points = [[-200, -50], [50, 100], [200, -50]]
 print(my_points[0][0],my_points[0][1])
 
-def draw_triangle(points,t):
+def draw_triangle(points,t,color):
+    t.fillcolor(color)
     t.up()
     t.goto(points[0][0],points[0][1])
     t.down()
@@ -13,11 +14,33 @@ def draw_triangle(points,t):
     t.goto(points[0][0], points[0][1])
     t.end_fill()
 
+def find_middle(pointA,pointB):
+    return (pointA[0]+pointB[0])/2,(pointA[1]+pointB[1])/2
+
+
+def sierpinski(points,turtle,degree):
+    color_map = ['blue', 'red', 'green', 'white', 'yellow', 'violet', 'orange']
+    draw_triangle(points,turtle,color_map[degree])
+    if degree > 0:
+        sierpinski([points[0],
+                    find_middle(points[0],points[1]),
+                    find_middle(points[0],points[2])],
+                   turtle,
+                   degree-1)
+        sierpinski([points[1],
+                    find_middle(points[0],points[1]),
+                    find_middle(points[1],points[2])],
+                    turtle,degree-1)
+        sierpinski([points[2],
+                    find_middle(points[2],points[1]),
+                    find_middle(points[0],points[2])],turtle,degree-1)
+
+
+
 
 t= turtle.Turtle()
 my_win = turtle.Screen()
-t.color('green')
-draw_triangle(my_points,t)
+sierpinski(my_points,t,3)
 my_win.exitonclick()
 
 
